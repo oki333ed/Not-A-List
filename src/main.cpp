c7cf314f47e7ce791087c23348ea10df2406822a
oki3ed... <kokosik3215@gmail.com>
2026-03-18 14:13:01 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,32 @@
 using namespace geode::prelude;
 
 #include <Geode/modify/CreatorLayer.hpp>
-class $modify(MyCreatorLayer, CreatorLayer) {
+class $modify(CreatorLayer) {
 	bool init() {
 		if (!CreatorLayer::init()) {
 			return false;
 		}
 
 		auto menu = this->getChildByID("creator-buttons-menu");
+	    
+	    auto listBtn = menu->getChildByID("lists-button");
+
+		listBtn->setVisible(false);
+
+		menu->updateLayout();
+
+		return true;
+	}
+};
+
+#include <Geode/modify/LevelSearchLayer.hpp>
+class $modify(LevelSearchLayer) {
+	bool init() {
+		if (!LevelSearchLayer::init()) {
+			return false;
+		}
+
+		auto menu = this->getChildByID("other-filter-menu");
 	    
 	    auto listBtn = menu->getChildByID("lists-button");
 
